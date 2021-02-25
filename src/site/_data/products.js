f8b99a982c8e8de471a431d90da7a9712dc9d33f
Panca Satrio Nugroho <nugroho.panca@gmail.com>
2021-02-25 10:54:01 +0700

    Update 25 Feb 2021

---
+++ src/site/_data/products.js
@@ -0,0 +1,156 @@
+module.exports = {
+    range: [
+        {
+            name: 'HINO300 Dutro',
+            image: 'assets/uploads/products/dutro-110-sd6.png',
+            logo:'assets/uploads/categories_child/imageedit_34_5121943756.png',
+            available_categories: ['Cargo','Dump','Mixer'],
+            products: [
+                {
+                    type: 'Truk',
+                    series: 'Dutro 110 SD',
+                    image: 'assets/uploads/products/dutro-110-sd6.png',
+                    category: 'Cargo'
+                },
+                {
+                    type: 'Truk',
+                    series: 'Dutro 110 LDL',
+                    image: 'assets/uploads/products/New_Dutro_MDL_Box1.png',
+                    category: 'Cargo'
+                },
+                {
+                    type: 'Truk',
+                    series: 'Dutro 130 HD',
+                    image: 'assets/uploads/products/New_Dutro_HD_Dump_02_02.png',
+                    category: 'Dump'
+                },
+                {
+                    type: 'Truk',
+                    series: 'Dutro 130 HD LSD',
+                    image: 'assets/uploads/products/New_Dutro_130_HDL_NEW-DI3.png',
+                    category: 'Dump'
+                },
+                {
+                    type: 'Truk',
+                    series: 'Dutro 130 HD Mixer',
+                    image: 'assets/uploads/products/dutro-130-hdm.png',
+                    category: 'Mixer'
+                }
+            ]
+        },
+        {
+            name: 'HINO500 Ranger',
+            image: 'assets/uploads/products/EX_scooper_Revisi_514HFRIDN001D3.png',
+            logo:'assets/uploads/categories_child/rangerlogo1.png',
+            available_categories: ['Cargo','Dump','Mixer','Tractor Head'],
+            products: [
+                {
+                    type: 'Truk',
+                    series: 'FG 235 JU',
+                    image: 'assets/uploads/products/FG235JU.png',
+                    category: 'Cargo'
+                },
+                {
+                    type: 'Truk',
+                    series: 'FM 260 JD (Mining)',
+                    image: 'assets/uploads/products/EX_rigid_Revisi_514LFRIDN001D12.png',
+                    category: 'Dump'
+                },
+                {
+                    type: 'Truk',
+                    series: 'FM 350 PD (Mining)',
+                    image: 'assets/uploads/products/EX_scooper_Revisi_514HFRIDN001D3.png',
+                    category: 'Dump'
+                },
+                {
+                    type: 'Truk',
+                    series: 'FM 285 JM',
+                    image: 'assets/uploads/products/EX_mixer_514LFRIDN001M3.png',
+                    category: 'Mixer'
+                },
+                {
+                    type: 'Truk',
+                    series: 'SG 285 T/H (with ABS system)',
+                    image: 'assets/uploads/products/EX_carrier_514LNRIDN001T_Revisi-Win-Def9.png',
+                    category: 'Tractor Head'
+                }
+            ]
+        },
+        {
+            name: 'HINO700 Profia',
+            image: 'assets/uploads/products/SS1E-01.png',
+            logo: 'assets/uploads/categories_child/profia_logo1.png',
+            available_categories: ['SS Series 6X4','SV Series 6X4','ZS Series 6X4','ZY Series 8X4'],
+            products: [
+                {
+                    type: 'Truk',
+                    series: 'SS1E TH',
+                    image: 'assets/uploads/products/SS1E-01.png',
+                    category: 'SS Series 6X4'
+                },
+                {
+                    type: 'Truk',
+                    series: 'SV 4145',
+                    image: 'assets/uploads/products/SV.png',
+                    category: 'SV Series 6X4'
+                },
+                {
+                    type: 'Truk',
+                    series: 'ZS 4141',
+                    image: 'assets/uploads/products/Hino-700_Profia-ZS.png',
+                    category: 'ZS Series 6X4'
+                },
+                {
+                    type: 'Truk',
+                    series: 'ZY 5041',
+                    image: 'assets/uploads/products/ZY6.png',
+                    category: 'ZY Series 8X4'
+                }
+            ]
+        },
+        {
+            name: 'HINO Bus',
+            image: 'assets/uploads/products/sdbl31.png',
+            logo: 'assets/uploads/categories_child/bus_logo1.png',
+            available_categories: ['Bus Mikro','Bus Medium','Bus Besar'],
+            products: [
+                {
+                    type: 'Bus',
+                    series: 'Dutro Bus 110 SDB',
+                    image: 'assets/uploads/products/SS1E-01.png',
+                    category: 'Bus Mikro'
+                },
+                {
+                    type: 'Bus',
+                    series: 'Dutro Bus 110 SDBL',
+                    image: 'assets/uploads/products/sdbl31.png',
+                    category: 'Bus Mikro'
+                },
+                {
+                    type: 'Bus',
+                    series: 'Dutro Bus 130 MDBL',
+                    image: 'assets/uploads/products/sdb6.png',
+                    category: 'Bus Medium'
+                },
+                {
+                    type: 'Bus',
+                    series: 'FB 130',
+                    image: 'assets/uploads/products/fb.png',
+                    category: 'Bus Medium'
+                },
+                {
+                    type: 'Bus',
+                    series: 'FC Bus',
+                    image: 'assets/uploads/products/fc-bus-no-back.png',
+                    category: 'Bus Besar'
+                },
+                {
+                    type: 'Bus',
+                    series: 'RN 285 AT',
+                    image: 'assets/uploads/products/Bus_RM_285_Matic_GIIAS2.png',
+                    category: 'Bus Besar'
+                }
+            ]
+        },
+    ]
+}
